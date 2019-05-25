4ba94baca2c66e7347dba17dcf97d902267c17f0
Warren Yao <warrenyca905@gmail.com>
2019-05-25 21:37:25 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(d_name)
+    @name = d_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(d_breed)
+    @breed = d_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
